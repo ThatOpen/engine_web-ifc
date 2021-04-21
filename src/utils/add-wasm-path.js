88b9e592d122207d148d4a4767ee0b4b99afa8e8
Ansoni <antoniogviegas@hotmail.com>
2021-04-22 01:45:52 +0200

    Clean up: use modern JS only

--- src/utils/add-wasm-path.js
+++ src/utils/add-wasm-path.js
@@ -2,19 +2,19 @@
 
 const fs = require("fs");
 
-var buildFile = "dist/web-ifc-api.js";
-var wasmPathDeclaration = '\n\n var WasmPath = "";';
-var oldWasmPath = /var wasmBinaryFile = "web-ifc.wasm";/;
-var newWasmPath = 'var wasmBinaryFile = WasmPath + "web-ifc.wasm";';
+const buildFile = "dist/web-ifc-api.js";
+const wasmPathDeclaration = '\n\n var WasmPath = "";';
+const oldWasmPath = /var wasmBinaryFile = "web-ifc.wasm";/;
+const newWasmPath = 'var wasmBinaryFile = WasmPath + "web-ifc.wasm";';
 
-fs.appendFile(buildFile, wasmPathDeclaration, function (err) {
+fs.appendFile(buildFile, wasmPathDeclaration, (err) => {
   if (err) return console.log(err);
 });
 
 fs.readFile(buildFile, "utf8", (err, data) => {
   if (err) return console.error(err);
   
-  var result = data.replace(oldWasmPath, newWasmPath);
+  const result = data.replace(oldWasmPath, newWasmPath);
   fs.writeFile(buildFile, result, "utf8", (err) => {
     if (err) return console.log(err);
   });
