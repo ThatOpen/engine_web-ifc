5763589f81078983c0b29152b6f4d88f8c667c1e
Tom van Diggelen <info@tomvandig.com>
2021-08-26 20:31:46 +0200

    Add example for partial geometry stream

--- examples/usage/src/geometrystream.ts
+++ examples/usage/src/geometrystream.ts
@@ -30,4 +30,34 @@ export default async function() {
         Equals("total num vertices", totalSizeVerts, 181602);
         Equals("total num indices", totalSizeIndices, 43989);
     });
+
+    await WithIFCFileLoaded("partialgeometrystream", (ifcapi: WebIFC.IfcAPI, modelID: number, info: TestInfo) => {
+
+        let totalSizeVerts = 0;
+        let totalSizeIndices = 0;
+
+        // grab all types except IFCCOLUMN
+        let types = WebIFC.IfcElements.filter((f) => (f !== WebIFC.IFCCOLUMN));
+
+        ifcapi.StreamAllMeshesWithTypes(modelID, types, (mesh: WebIFC.FlatMesh) => {
+            
+            // only during the lifetime of this function call, the geometry is available in memory
+
+            const placedGeometries = mesh.geometries;
+
+            for (let i = 0; i < placedGeometries.size(); i++)
+            {
+                const placedGeometry = placedGeometries.get(i);
+                const geometry = ifcapi.GetGeometry(modelID, placedGeometry.geometryExpressID);
+                const verts = ifcapi.GetVertexArray(geometry.GetVertexData(), geometry.GetVertexDataSize());
+                const indices = ifcapi.GetIndexArray(geometry.GetIndexData(), geometry.GetIndexDataSize());
+
+                totalSizeVerts += verts.length;
+                totalSizeIndices += indices.length;
+            }
+        });
+
+        Equals("total num vertices", totalSizeVerts, 127482);
+        Equals("total num indices", totalSizeIndices, 31047);
+    });
 }
