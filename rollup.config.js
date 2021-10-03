df990f778805ca8fd068b3cabae64da15ca64c62
Rodrigo <hamuyrodrigo@gmail.com>
2021-10-03 15:40:22 +0100

    add build and dist folders

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from "./package.json";
+import fs from 'fs';
+
+fs.copyFileSync('wasm/build/web-ifc.wasm', 'dist/web-ifc.wasm');
 
 export default {
   input: 'src/index.ts',
