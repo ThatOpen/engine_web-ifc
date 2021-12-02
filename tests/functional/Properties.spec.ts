809cd445b6a0cd28df2f8b3e2e76339dc4c2c8f8
William Martin <awmartin@anomalus.com>
2021-12-02 10:42:03 -0500

    Access type aliases directly in Properties test

--- tests/functional/Properties.spec.ts
+++ tests/functional/Properties.spec.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as WebIFC from '../../dist/web-ifc-api-node.js';
+import * as IfcElements from "../../src/ifc2x4";
 import type { IfcAPI } from '../../dist/web-ifc-api-node.js';
 import type { Properties } from '../../dist/helpers/properties.js';
 
@@ -20,16 +21,12 @@ beforeAll(async () => {
 
 describe('Properties', () => {
     test('can get all IFCWALLSTANDARDCASE items', async () => {
-        // TODO Get IFCWALLSTANDARDCASE from the types map.
-        const IFCWALLSTANDARDCASE = 3512223829
-        const walls : any[] = await properties.getAllItemsOfType(modelID, IFCWALLSTANDARDCASE, false)
+        const walls : any[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false)
         expect(walls.length).toBe(17)
     })
 
     test('can get IFCWALLSTANDARDCASE details', async () => {
-        // TODO Get IFCWALLSTANDARDCASE from the types map.
-        const IFCWALLSTANDARDCASE = 3512223829
-        const allWalls : any[] = await properties.getAllItemsOfType(modelID, IFCWALLSTANDARDCASE, false)
+        const allWalls : any[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false)
         const firstWallId = allWalls[0]
         const line = await properties.getItemProperties(modelID, firstWallId)
         expect(line.Name.value).toEqual('Basic Wall:150 Concrete:677248')
