3ddb5dfd4e92a9937bf08ce65d8ab0340b65ecab
William Martin <awmartin@anomalus.com>
2021-12-02 10:34:22 -0500

    Refactor Properties test to use its interface

--- tests/functional/Properties.spec.ts
+++ tests/functional/Properties.spec.ts
@@ -2,9 +2,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as WebIFC from '../../dist/web-ifc-api-node.js';
 import type { IfcAPI } from '../../dist/web-ifc-api-node.js';
+import type { Properties } from '../../dist/helpers/properties.js';
 
 let modelID : number
 let ifcApi : IfcAPI
+let properties : Properties
+
 beforeAll(async () => {
     ifcApi = new WebIFC.IfcAPI();
     await ifcApi.Init();
@@ -12,22 +15,23 @@ beforeAll(async () => {
     const exampleIFCPath = path.join(__dirname, '../artifacts/example.ifc.test');
     const exampleIFCData = fs.readFileSync(exampleIFCPath);
     modelID = ifcApi.OpenModel(exampleIFCData);
+    properties = ifcApi.properties
 })
 
 describe('Properties', () => {
     test('can get all IFCWALLSTANDARDCASE items', async () => {
         // TODO Get IFCWALLSTANDARDCASE from the types map.
         const IFCWALLSTANDARDCASE = 3512223829
-        const walls : any[] = await ifcApi.properties.getAllItemsOfType(modelID, IFCWALLSTANDARDCASE, false)
+        const walls : any[] = await properties.getAllItemsOfType(modelID, IFCWALLSTANDARDCASE, false)
         expect(walls.length).toBe(17)
     })
 
     test('can get IFCWALLSTANDARDCASE details', async () => {
         // TODO Get IFCWALLSTANDARDCASE from the types map.
         const IFCWALLSTANDARDCASE = 3512223829
-        const walls : any[] = await ifcApi.properties.getAllItemsOfType(modelID, IFCWALLSTANDARDCASE, false)
-        const firstWallId = walls[0]
-        const line = ifcApi.GetLine(modelID, firstWallId)
+        const allWalls : any[] = await properties.getAllItemsOfType(modelID, IFCWALLSTANDARDCASE, false)
+        const firstWallId = allWalls[0]
+        const line = await properties.getItemProperties(modelID, firstWallId)
         expect(line.Name.value).toEqual('Basic Wall:150 Concrete:677248')
     })
 })
