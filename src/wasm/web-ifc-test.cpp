2db8c1431e5d9469da3722e01ab9872ee564101b
QuimMoya <q.moya@hotmail.com>
2022-04-07 10:05:11 +0200

    Revert "Testing 102"
    This reverts commit c0eb1982dc874d4be4b515521b0d1f9ddacea552.

--- src/wasm/web-ifc-test.cpp
+++ src/wasm/web-ifc-test.cpp
@@ -229,7 +229,7 @@ int main()
 
     //return 0;
 
-    std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#102/M3D-CON-CD.ifc");
+    std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#21/Mini Project.ifc");
     //std::string content = ReadFile(L"D:/web-ifc-obb/benchmark/ifcfiles/test.ifc");
     //std::string content = ReadFile(L"D:/web-ifc/src/wasm/build/output.ifc");
 
@@ -268,8 +268,8 @@ int main()
     webifc::IfcGeometryLoader geometryLoader(loader);
 
     start = webifc::ms();
-    //SpecificLoadTest(loader, geometryLoader, 524);
-    auto meshes = LoadAllTest(loader, geometryLoader);
+    SpecificLoadTest(loader, geometryLoader, 524);
+    //auto meshes = LoadAllTest(loader, geometryLoader);
     auto trans = webifc::FlattenTransformation(geometryLoader.GetCoordinationMatrix());
 
     auto errors = loader.GetAndClearErrors();
