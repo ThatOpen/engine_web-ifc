30ddef7837265d74b8e55545fbcc5352dc6dc9e6
QuimMoya <q.moya@hotmail.com>
2022-08-01 14:34:04 +0200

    Improvement circular side

--- src/wasm/web-ifc-test.cpp
+++ src/wasm/web-ifc-test.cpp
@@ -226,10 +226,10 @@ int main()
 
     // return 0;
 
-    // std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#145 Solved/S_Office_Integrated Design Archi.ifc");
+    std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#145 Solved/S_Office_Integrated Design Archi.ifc");
     // std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#156 Solved/#172 Processing/15_testing.ifc");
     // std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#156 Solved/BSI_EXAMPLES/advanced_brep_1.ifc");
-    std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#156 Solved/#172 Processing/15_testing.ifc");
+    // std::string content = ReadFile(L"C:/Users/qmoya/Desktop/PROGRAMES/VSCODE/IFC.JS/issues/#156 Solved/#172 Processing/15_testing.ifc");
 
     webifc::LoaderSettings set;
     set.COORDINATE_TO_ORIGIN = true;
@@ -263,8 +263,9 @@ int main()
     webifc::IfcGeometryLoader geometryLoader(loader);
 
     start = webifc::ms();
-    // SpecificLoadTest(loader, geometryLoader, 414835);
-    SpecificLoadTest(loader, geometryLoader, 30782);
+    SpecificLoadTest(loader, geometryLoader, 414835);
+    // SpecificLoadTest(loader, geometryLoader, 30782);
+    // SpecificLoadTest(loader, geometryLoader, 217102);
     // SpecificLoadTest(loader, geometryLoader, 181);
     // auto meshes = LoadAllTest(loader, geometryLoader);
     auto trans = webifc::FlattenTransformation(geometryLoader.GetCoordinationMatrix());
