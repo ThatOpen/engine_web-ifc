5627e4a4645fb9213bba43e1a6aefbd3650f8118
TxTony <t.tipveau@berd.re>
2022-10-09 22:42:43 +0400

    beautify

--- tests/functional/Properties.spec.ts
+++ tests/functional/Properties.spec.ts
@@ -2,12 +2,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as WebIFC from '../../dist/web-ifc-api-node.js';
 import * as IfcElements from "../../src/ifc2x4";
-import type { IfcAPI } from '../../dist/web-ifc-api-node.js';
-import type { Properties } from '../../dist/helpers/properties.js';
+import type {
+    IfcAPI
+} from '../../dist/web-ifc-api-node.js';
+import type {
+    Properties
+} from '../../dist/helpers/properties.js';
 
-let modelID : number
-let ifcApi : IfcAPI
-let properties : Properties
+let modelID: number
+let ifcApi: IfcAPI
+let properties: Properties
 
 beforeAll(async () => {
     ifcApi = new WebIFC.IfcAPI();
@@ -21,47 +25,47 @@ beforeAll(async () => {
 
 describe('Properties', () => {
     test('can get all IFCWALLSTANDARDCASE items', async () => {
-        const walls : any[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false)
+        const walls: any[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false)
         expect(walls.length).toBe(17)
     })
 
     test('can get IFCWALLSTANDARDCASE details', async () => {
-        const allWalls : any[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false)
+        const allWalls: any[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false)
         const firstWallId = allWalls[0]
         const line = await properties.getItemProperties(modelID, firstWallId)
         expect(line.Name.value).toEqual('Basic Wall:150 Concrete:677248')
     })
-    
+
     test('can get IFC type from integer', async () => {
-        const wallsStandard  =  properties.getIfcType(IfcElements.IFCWALLSTANDARDCASE);
+        const wallsStandard = properties.getIfcType(IfcElements.IFCWALLSTANDARDCASE);
         expect(wallsStandard).toEqual("IFCWALLSTANDARDCASE")
     })
 
     test('can get property sets', async () => {
-        const propertySets  = await  properties.getPropertySets(modelID, 9989);
+        const propertySets = await properties.getPropertySets(modelID, 9989);
         expect(propertySets.length).toEqual(4);
     })
 
     test('can get property sets with children datas', async () => {
-        const propertySets  = await  properties.getPropertySets(modelID, 9989, true);
+        const propertySets = await properties.getPropertySets(modelID, 9989, true);
         let HasProperties = propertySets[0]["HasProperties"].length;
         expect(HasProperties > 0).toBe(true);
     })
 
     test('can get property sets with children datas ', async () => {
-        const propertySets  = await  properties.getPropertySets(modelID, 9989, true);
+        const propertySets = await properties.getPropertySets(modelID, 9989, true);
         let HasProperties = propertySets[0]["HasProperties"].length;
         expect(HasProperties > 0).toBe(true);
     })
 
     test('can get property materials on one given element', async () => {
-        const propertyMaterials  = await  properties.getMaterialsProperties(modelID, 10258);
+        const propertyMaterials = await properties.getMaterialsProperties(modelID, 10258);
         expect(propertyMaterials[0]["Name"]["value"]).toEqual('Metal - Steel - 345 MPa');
     })
 
     // this test stink it needs to be reviewed
     test('can get spatial structure ', async () => {
-        const spatialStructure : any = await  properties.getSpatialStructure(modelID);
+        const spatialStructure: any = await properties.getSpatialStructure(modelID);
         let site = spatialStructure.children[0];
         let building = site.children[0];
         let storey = building.children[0];
@@ -74,7 +78,7 @@ describe('Properties', () => {
     })
 
     test('can get spatial structure including properties', async () => {
-        const spatialStructure : any = await  properties.getSpatialStructure(modelID,true);
+        const spatialStructure: any = await properties.getSpatialStructure(modelID, true);
         let site = spatialStructure.children[0];
         let building = site.children[0];
         let storey = building.children[0];
@@ -83,16 +87,16 @@ describe('Properties', () => {
     })
 
     test('can get all items of a given type', async () => {
-        const IFCWALLSTANDARDCASEITEMS : number[] = await  properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false);
+        const IFCWALLSTANDARDCASEITEMS: number[] = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, false);
         expect(IFCWALLSTANDARDCASEITEMS.length).toEqual(17);
     })
 
     test('can get all items of a given type with more details ( verbose )', async () => {
-        const IFCWALLSTANDARDCASEITEMS : any = await  properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, true);
+        const IFCWALLSTANDARDCASEITEMS: any = await properties.getAllItemsOfType(modelID, IfcElements.IFCWALLSTANDARDCASE, true);
         expect(IFCWALLSTANDARDCASEITEMS.length).toEqual(17);
         expect(IFCWALLSTANDARDCASEITEMS[0].hasOwnProperty("GlobalId")).toBeTruthy();
     })
-    
+
 })
 
 afterAll(() => {
--- tests/functional/WebIfcApi.spec.ts
+++ tests/functional/WebIfcApi.spec.ts
@@ -1,23 +1,33 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as WebIFC from '../../dist/web-ifc-api-node.js';
-import type { Vector, FlatMesh, IfcAPI, IfcExtrudedAreaSolid, RawLineData } from '../../dist/web-ifc-api-node.js';
-import { IfcGeometry, IfcThermalExpansionCoefficientMeasure, LoaderError } from 'web-ifc';
+import type {
+    Vector,
+    FlatMesh,
+    IfcAPI,
+    IfcExtrudedAreaSolid,
+    RawLineData
+} from '../../dist/web-ifc-api-node.js';
+import {
+    IfcGeometry,
+    IfcThermalExpansionCoefficientMeasure,
+    LoaderError
+} from 'web-ifc';
 
 
-let ifcApi : IfcAPI;
-let modelID : number ;
-let expressId : number = 9989; // an IFCSPACE
-let geometries : Vector<FlatMesh> ; // to store geometries instead of refetching them
-let allGeometriesSize : number = 119;
-let quantityOfknownErrors : number = 0;
-let meshesCount : number = 115;
-let emptyFileModelID : number;
+let ifcApi: IfcAPI;
+let modelID: number;
+let expressId: number = 9989; // an IFCSPACE
+let geometries: Vector < FlatMesh > ; // to store geometries instead of refetching them
+let allGeometriesSize: number = 119;
+let quantityOfknownErrors: number = 0;
+let meshesCount: number = 115;
+let emptyFileModelID: number;
 let expressIDMatchingGuid: any = {
-    expressID:2863,
-    guid:"0VNYAWfXv8JvIRVfOzYH1j"
+    expressID: 2863,
+    guid: "0VNYAWfXv8JvIRVfOzYH1j"
 }
-let IFCEXTRUDEDAREASOLIDMeshesCount = 97; 
+let IFCEXTRUDEDAREASOLIDMeshesCount = 97;
 
 beforeAll(async () => {
     ifcApi = new WebIFC.IfcAPI();
@@ -34,7 +44,7 @@ describe('WebIfcApi reading methods', () => {
         expect(modelID).toBe(0);
     })
     test('can ensure model is open', () => {
-        const isOpen : boolean = ifcApi.IsModelOpen(modelID);
+        const isOpen: boolean = ifcApi.IsModelOpen(modelID);
         expect(isOpen).toBeTruthy();
     })
     test('can return the correct number of line with a given Type', () => {
@@ -42,41 +52,41 @@ describe('WebIfcApi reading methods', () => {
         expect(properties.size()).toEqual(456);
     })
     test('can return the correct number of line when getting all lines', () => {
-        const lines : any = ifcApi.GetAllLines(modelID);
+        const lines: any = ifcApi.GetAllLines(modelID);
         expect(lines.size()).toEqual(6487);
     })
     test('can GetLine', () => {
-        const line : Object = ifcApi.GetLine(modelID, expressId);
+        const line: Object = ifcApi.GetLine(modelID, expressId);
         expect(line).not.toBeNull();
     })
     test('can flatten a line which will be more verbose', () => {
-        const line : any = ifcApi.GetLine(modelID, expressId);
+        const line: any = ifcApi.GetLine(modelID, expressId);
         ifcApi.FlattenLine(modelID, line);
         expect(line.OwnerHistory.OwningUser).not.toBe(null);
     })
     test('expect the correct line to be returned', () => {
-        const line : any = ifcApi.GetLine(modelID, expressId);
+        const line: any = ifcApi.GetLine(modelID, expressId);
         expect(line.expressID).toEqual(expressId);
     })
     test('expect getting flatten line return verbose line', () => {
-        let flattened : boolean = true;
-        const line : any = ifcApi.GetLine(modelID, expressId, flattened);
+        let flattened: boolean = true;
+        const line: any = ifcApi.GetLine(modelID, expressId, flattened);
         expect(line.Representation.Representations.length).toEqual(2); // hardcoded
     })
     test('can Get Raw Line', () => {
-        let line : RawLineData = ifcApi.GetRawLineData(modelID, expressId);
+        let line: RawLineData = ifcApi.GetRawLineData(modelID, expressId);
         expect(line).not.toBeNull();
     })
     test('expect the correct raw line to be returned', () => {
-        let line : RawLineData = ifcApi.GetRawLineData(modelID, expressId);
+        let line: RawLineData = ifcApi.GetRawLineData(modelID, expressId);
         expect(line.ID).toEqual(expressId);
     })
     test('can count errors in ifc file', () => {
-        let errors : any = ifcApi.GetAndClearErrors(modelID);
+        let errors: any = ifcApi.GetAndClearErrors(modelID);
         expect(errors.size()).toEqual(quantityOfknownErrors);
-    }) 
+    })
     test('can Create Ifc Guid To Express Id Map', () => {
-        let count : number = 0;
+        let count: number = 0;
         ifcApi.CreateIfcGuidToExpressIdMapping(modelID);
         expect(ifcApi.ifcGuidMap.get(0)?.get(expressIDMatchingGuid.expressID)).toEqual(expressIDMatchingGuid.guid);
     })
@@ -91,7 +101,8 @@ describe('WebIfcApi geometries', () => {
     })
     test('can Get a Flat Mesh', () => {
         let geometryExpressId = geometries.get(1).expressID;
-        let flatMesh : FlatMesh  = ifcApi.GetFlatMesh(modelID, geometryExpressId);
+        let flatMesh: FlatMesh = ifcApi.GetFlatMesh(modelID, geometryExpressId);
+        console.log(geometryExpressId);
         expect(flatMesh.geometries.size() > 0).toBeTruthy();
     })
     test('expect the correct flatMesh to be returned', () => {
@@ -101,57 +112,57 @@ describe('WebIfcApi geometries', () => {
         expect(flatMesh.expressID).toEqual(geometryExpressId);
     })
     test('can ensure the corret number of all streamed meshes ', () => {
-        let count : number = 0;
-        ifcApi.StreamAllMeshes(modelID,(mesh)=>{
+        let count: number = 0;
+        ifcApi.StreamAllMeshes(modelID, (mesh) => {
             count++;
         });
         expect(count).toEqual(meshesCount);
     })
     test('can ensure the corret number of all streamed meshes with a given Types', () => {
-        let count : number = 0;
-        ifcApi.StreamAllMeshesWithTypes(modelID, [WebIFC.IFCEXTRUDEDAREASOLID],(mesh)=>{
-           ++count
+        let count: number = 0;
+        ifcApi.StreamAllMeshesWithTypes(modelID, [WebIFC.IFCEXTRUDEDAREASOLID], (mesh) => {
+            ++count
         });
         expect(count).toEqual(IFCEXTRUDEDAREASOLIDMeshesCount);
-    })  
+    })
     test('can get geometry', () => {
         let geometryId = geometries.get(1).expressID;
-        const geometry : IfcGeometry = ifcApi.GetGeometry(modelID,geometryId);
+        const geometry: IfcGeometry = ifcApi.GetGeometry(modelID, geometryId);
         expect(geometry.GetIndexData()).not.toBeNull()
         expect(geometry.GetIndexDataSize()).not.toBeNull()
         expect(geometry.GetVertexData()).not.toBeNull()
         expect(geometry.GetVertexDataSize()).not.toBeNull()
     })
     test('can Get Coordination Matrix', () => {
-        let coordinationMatrix : Array<number> = ifcApi.GetCoordinationMatrix(modelID);
+        let coordinationMatrix: Array < number > = ifcApi.GetCoordinationMatrix(modelID);
         expect(coordinationMatrix.join(",")).toEqual("1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1");
     })
     test('expect vertexArray as Float32Array', () => {
         let geometryId = geometries.get(1).expressID;
-        const geometry : IfcGeometry = ifcApi.GetGeometry(modelID,geometryId);
-        let vertexArray : Float32Array = ifcApi.GetVertexArray(geometry.GetVertexData(),geometry.GetVertexDataSize());
+        const geometry: IfcGeometry = ifcApi.GetGeometry(modelID, geometryId);
+        let vertexArray: Float32Array = ifcApi.GetVertexArray(geometry.GetVertexData(), geometry.GetVertexDataSize());
         expect(vertexArray).toBeInstanceOf(Float32Array);
     })
     // this test must be reviewed!
     test('can return vertex array from a flat mesh', () => {
         let geometryExpressId = geometries.get(1).expressID;
         let flatMesh = ifcApi.GetFlatMesh(modelID, geometryExpressId);
-        let flatMesheGeometries : IfcGeometry = ifcApi.GetGeometry(modelID, flatMesh.geometries.get(0).geometryExpressID);
-        let geometryVertexArray : Float32Array = ifcApi.GetVertexArray(flatMesheGeometries.GetVertexData(), flatMesheGeometries.GetVertexDataSize());
+        let flatMesheGeometries: IfcGeometry = ifcApi.GetGeometry(modelID, flatMesh.geometries.get(0).geometryExpressID);
+        let geometryVertexArray: Float32Array = ifcApi.GetVertexArray(flatMesheGeometries.GetVertexData(), flatMesheGeometries.GetVertexDataSize());
         expect(geometryVertexArray.length > 0).toBeTruthy();
     })
     test('expect index array as Float32Array', () => {
         let geometryId = geometries.get(1).expressID;
-        const geometry : IfcGeometry = ifcApi.GetGeometry(modelID,geometryId);
-        let indexArray : Float32Array = ifcApi.GetVertexArray(geometry.GetIndexData(),geometry.GetIndexDataSize());
+        const geometry: IfcGeometry = ifcApi.GetGeometry(modelID, geometryId);
+        let indexArray: Float32Array = ifcApi.GetVertexArray(geometry.GetIndexData(), geometry.GetIndexDataSize());
         expect(indexArray).toBeInstanceOf(Float32Array);
     })
     // this test must be reviewed!
     test('can return geometry index datas from a flat mesh', () => {
         let geometryExpressId = geometries.get(1).expressID;
         let flatMesh = ifcApi.GetFlatMesh(modelID, geometryExpressId);
-        let flatMesheGeometries : IfcGeometry = ifcApi.GetGeometry(modelID, flatMesh.geometries.get(0).geometryExpressID);
-        let geometryIndexData : Uint32Array = ifcApi.GetIndexArray(flatMesheGeometries.GetIndexData(), flatMesheGeometries.GetIndexDataSize());
+        let flatMesheGeometries: IfcGeometry = ifcApi.GetGeometry(modelID, flatMesh.geometries.get(0).geometryExpressID);
+        let geometryIndexData: Uint32Array = ifcApi.GetIndexArray(flatMesheGeometries.GetIndexData(), flatMesheGeometries.GetIndexDataSize());
         expect(geometryIndexData.length > 0).toBeTruthy();
     })
 });
@@ -170,80 +181,138 @@ describe('WebIfcApi writing methods', () => {
         expect(emptyFileModelID).toBe(1);
     })
     test('Can write a line from raw datas', () => {
-        ifcApi.WriteRawLineData(emptyFileModelID,{
+        ifcApi.WriteRawLineData(emptyFileModelID, {
             ID: 1,
             type: 4251960020,
-            arguments: [
-              { type: 1, value: '0cqv$41Df7ygSQzJTMBnvM' },
-              { type: 1, value: '' },
-              { type: 1, value: 'foo' },
-              null,
-              { type: 1, value: '' },
-              { type: 1, value: '' },
-              { type: 1, value: '' },
-              { type: 1, value: '' }
+            arguments: [{
+                    type: 1,
+                    value: '0cqv$41Df7ygSQzJTMBnvM'
+                },
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: 'foo'
+                },
+                null,
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: ''
+                }
             ]
         })
-        let project : any = ifcApi.GetLine(emptyFileModelID,1);
+        let project: any = ifcApi.GetLine(emptyFileModelID, 1);
         expect(project.Description.value).toEqual("foo");
     })
     test('Can modify a line with a rawLineData', () => {
-        ifcApi.WriteRawLineData(emptyFileModelID,{
+        ifcApi.WriteRawLineData(emptyFileModelID, {
             ID: 1,
             type: 4251960020,
-            arguments: [
-              { type: 1, value: '0cqv$41Df7ygSQzJTMBnvM' },
-              { type: 1, value: '' },
-              { type: 1, value: 'foobar' },
-              null,
-              { type: 5, value: '' },
-              { type: 1, value: '' },
-              { type: 1, value: '' },
-              { type: 1, value: '' }
+            arguments: [{
+                    type: 1,
+                    value: '0cqv$41Df7ygSQzJTMBnvM'
+                },
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: 'foobar'
+                },
+                null,
+                {
+                    type: 5,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: ''
+                },
+                {
+                    type: 1,
+                    value: ''
+                }
             ]
         })
-        let project : any = ifcApi.GetLine(emptyFileModelID,1);
+        let project: any = ifcApi.GetLine(emptyFileModelID, 1);
         expect(project.Description.value).toBe("foobar");
     })
-    test('can write a line by giving a line object',() =>{
-        let anIfcSpace : any = ifcApi.GetLine(modelID,9989);
-        let projectBeforeWriting : any = ifcApi.GetAllLines(modelID);
+    test('can write a line by giving a line object', () => {
+        let anIfcSpace: any = ifcApi.GetLine(modelID, 9989);
+        let projectBeforeWriting: any = ifcApi.GetAllLines(modelID);
         let payload = {
-            expressID:9999999,
+            expressID: 9999999,
             type: 3856911033,
             toType: 3856911033,
-            OwnerHistory: { type: 5, value: 41 },
-            Name: { type: 1, value: 'NZ-SHS beam:100x6.0SHS:823947' },
+            OwnerHistory: {
+                type: 5,
+                value: 41
+            },
+            Name: {
+                type: 1,
+                value: 'NZ-SHS beam:100x6.0SHS:823947'
+            },
             Description: null,
-            ObjectType: { type: 1, value: 'NZ-SHS beam:100x6.0SHS' },
-            ObjectPlacement: { type: 5, value: 9750 },
-            Representation: { type: 5, value: 9987 },
-            LongName: { type: 1, value: '823947' },
+            ObjectType: {
+                type: 1,
+                value: 'NZ-SHS beam:100x6.0SHS'
+            },
+            ObjectPlacement: {
+                type: 5,
+                value: 9750
+            },
+            Representation: {
+                type: 5,
+                value: 9987
+            },
+            LongName: {
+                type: 1,
+                value: '823947'
+            },
             CompositionType: undefined,
             PredefinedType: undefined,
             ElevationWithFlooring: undefined
         }
-        
-        ifcApi.WriteLine(modelID,payload);
-        let projectAfterWriting : any = ifcApi.GetAllLines(modelID);
-        expect(projectBeforeWriting.size()+1).toEqual(projectAfterWriting.size());
+
+        ifcApi.WriteLine(modelID, payload);
+        let projectAfterWriting: any = ifcApi.GetAllLines(modelID);
+        expect(projectBeforeWriting.size() + 1).toEqual(projectAfterWriting.size());
     })
-    test('can modify a line by giving a line object',() =>{
-        let project : any = ifcApi.GetLine(modelID,1);
+    test('can modify a line by giving a line object', () => {
+        let project: any = ifcApi.GetLine(modelID, 1);
         project.Name.value = "foo";
-        ifcApi.WriteLine(modelID,project);
-        let projectReloaded : any = ifcApi.GetLine(modelID,1);
+        ifcApi.WriteLine(modelID, project);
+        let projectReloaded: any = ifcApi.GetLine(modelID, 1);
         expect(projectReloaded.Name.value).toEqual("foo");
     })
-    test('can Export File As IFC',() =>{
-        let ifcDatas =  ifcApi.ExportFileAsIFC(modelID);
-        let exportModelID= ifcApi.OpenModel(ifcDatas);
-        const line : any = ifcApi.GetLine(exportModelID, expressId);
+    test('can Export File As IFC', () => {
+        let ifcDatas = ifcApi.ExportFileAsIFC(modelID);
+        let exportModelID = ifcApi.OpenModel(ifcDatas);
+        const line: any = ifcApi.GetLine(exportModelID, expressId);
         expect(exportModelID).toEqual(2);
         expect(line.expressID).toEqual(expressId);
-        
+
     })
-    
+
 })
 
 
@@ -252,7 +321,7 @@ describe('WebIfcApi known failures', () => {
         let ifcApi = new WebIFC.IfcAPI();
         await ifcApi.Init();
         let failModelID = 0;
-        const exampleIFCPath : string = path.join(__dirname, '../artifacts/Sample_entities.ifc');
+        const exampleIFCPath: string = path.join(__dirname, '../artifacts/Sample_entities.ifc');
         const exampleIFCData = fs.readFileSync(exampleIFCPath);
         failModelID = ifcApi.OpenModel(exampleIFCData);
         const IFCELECTRICDISTRIBUTIONPOINT_EXPRESSID = 237;
@@ -264,7 +333,7 @@ describe('WebIfcApi known failures', () => {
         } catch (error) {
             expect(true).toBeTruthy();
         }
-        
+
         //expect(f).toThrow("FromRawLineData[rawLineData.type] is not a function"); // fail if error doesnt exist anymore ;) 
         ifcApi.CloseModel(failModelID);
     });
@@ -277,14 +346,14 @@ describe('WebIfcApi known failures', () => {
         };
         await ifcApi.Init();
         let failModelID = 0;
-        const exampleIFCPath : string = path.join(__dirname, '../artifacts/S_Office_Integrated Design Archi.ifc');
+        const exampleIFCPath: string = path.join(__dirname, '../artifacts/S_Office_Integrated Design Archi.ifc');
         const exampleIFCData = fs.readFileSync(exampleIFCPath);
         try {
             ifcApi.OpenModel(exampleIFCData, config);
             expect(true).toBeFalsy();
         } catch (error) {
             expect(true).toBeTruthy();
-        } 
+        }
         ifcApi.CloseModel(failModelID);
     });
 
@@ -293,8 +362,8 @@ describe('WebIfcApi known failures', () => {
 
 afterAll(() => {
     ifcApi.CloseModel(modelID);
-    const isOpen : boolean = ifcApi.IsModelOpen(modelID);
-    const isOpenEmptyFileModelID : boolean = ifcApi.IsModelOpen(emptyFileModelID);
+    const isOpen: boolean = ifcApi.IsModelOpen(modelID);
+    const isOpenEmptyFileModelID: boolean = ifcApi.IsModelOpen(emptyFileModelID);
     expect(isOpen).toBeFalsy()
     expect(isOpenEmptyFileModelID).toBeFalsy()
 })
