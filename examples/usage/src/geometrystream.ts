a60a52aa77efec32b201d2808336bb80010e3149
Tom Beach <beachth@cf.ac.uk>
2023-02-15 16:15:30 +0000

    Fix Tests

--- examples/usage/src/geometrystream.ts
+++ examples/usage/src/geometrystream.ts
@@ -26,8 +26,8 @@ export default async function() {
             }
         });
 
-        Equals("total num vertices", totalSizeVerts, 181656);
-        Equals("total num indices", totalSizeIndices, 43998);
+        Equals("total num vertices", totalSizeVerts, 181548);
+        Equals("total num indices", totalSizeIndices, 43980);
     });
 
     await WithIFCFileLoaded("partialgeometrystream", (ifcapi: WebIFC.IfcAPI, modelID: number) => {
@@ -56,7 +56,7 @@ export default async function() {
             }
         });
 
-        Equals("total num vertices", totalSizeVerts, 132168);
-        Equals("total num indices", totalSizeIndices, 32214);
+        Equals("total num vertices", totalSizeVerts, 132060);
+        Equals("total num indices", totalSizeIndices, 32196);
     });
 }
