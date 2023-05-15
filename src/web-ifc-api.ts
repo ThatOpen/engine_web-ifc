a7b02263ed46621e0a0be48f5e1428a9504343ba
Tom Beach <beachth@cf.ac.uk>
2023-05-15 14:58:29 +0100

    Remove Debugging

--- src/web-ifc-api.ts
+++ src/web-ifc-api.ts
@@ -30,9 +30,9 @@ let WebIFCWasm: any;
 
 if (typeof self !== 'undefined' && self.crossOriginIsolated) {
     try {
-      WebIFCWasm = require("./web-ifc-mt");
+        WebIFCWasm = require("./web-ifc-mt");
     } catch (ex){
-        WebIFCWASM = require(__WASM_PATH__);
+        WebIFCWasm = require(__WASM_PATH__);
     }
 } else WebIFCWasm = require(__WASM_PATH__);
 
@@ -180,7 +180,6 @@ export class IfcAPI {
      * you override the path from which the wasm module is loaded.
      */
     async Init(customLocateFileHandler?: LocateFileHandlerFn) {
-        console.log(WebIFCWasm);
         if (WebIFCWasm) {
             let locateFileHandler: LocateFileHandlerFn = (path, prefix) => {
                 // when the wasm module requests the wasm file, we redirect to include the user specified path
