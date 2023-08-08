854b9dcd8cbfab5722d9627958ccf7cf132a25fe
Tom Beach <beachth@cf.ac.uk>
2023-08-08 20:44:18 +0100

    Update encoding_test.cpp

--- src/wasm/test/encoding_test.cpp
+++ src/wasm/test/encoding_test.cpp
@@ -8,44 +8,44 @@ using namespace std;
 
 TEST(EscapeSymbols)
 {
-    string_view str = "\\\\ ''"sv;
-    auto str = webifc::parsing::p21decode(str);
+    string_view strIn = "\\\\ ''"sv;
+    auto str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "\\ '");
 }
 
 TEST(EscapeXTest)
 {
-    string_view str ="see \\X\\A7 4.1"sv;
-    auto str = webifc::parsing::p21decode(str);
+    string_view strIn ="see \\X\\A7 4.1"sv;
+    auto str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "see § 4.1");
 }
 
 TEST(EscapeX2Test)
 {
-    string_view str ="\\X2\\03C0\\X0\\"sv;
-    auto str = webifc::parsing::p21decode(str);
+    string_view strIn ="\\X2\\03C0\\X0\\"sv;
+    auto str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "π");
     
-    str ="\\X2\\03B103B203B3\\X0\\"sv;
-    str = webifc::parsing::p21decode(str);
+    strIn ="\\X2\\03B103B203B3\\X0\\"sv;
+    str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "αβγ");
 }
 
 TEST(EscapeX4Test)
 {
-    string_view str ="\\X4\\0000041F0000044000000438000004320000043500000442000000200000041C0000043800000440\\X0\\"sv;
-    auto str = webifc::parsing::p21decode(str);
+    string_view strIn ="\\X4\\0000041F0000044000000438000004320000043500000442000000200000041C0000043800000440\\X0\\"sv;
+    auto str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "Привет Мир");
 }
 
 TEST(EscapeSTest)
 {
-    string_view str ="\\S\\Drger"sv;
-    auto str = webifc::parsing::p21decode(str);
+    string_view strIn ="\\S\\Drger"sv;
+    auto str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "Ärger");
 
-    str = "h\\S\\ttel"sv;
-    str = webifc::parsing::p21decode(str);
+    strIn = "h\\S\\ttel"sv;
+    str = webifc::parsing::p21decode(strIn);
     ASSERT_EQ(str, "hôtel");
 }
 
