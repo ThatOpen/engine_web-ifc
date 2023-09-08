805658a2662306e3e7470b40f01b28c5c9b55102
Tom Beach <beachth@cf.ac.uk>
2023-09-08 21:39:41 +0200

    Update IfcTokenChunk.cpp

--- src/wasm/parsing/IfcTokenChunk.cpp
+++ src/wasm/parsing/IfcTokenChunk.cpp
@@ -65,9 +65,11 @@ namespace webifc::parsing
   
   void IfcTokenStream::IfcTokenChunk::Load()
   {
+      _loaded=true;
       if (_fileStream->GetRef()!=_fileStartRef) _fileStream->Go(_fileStartRef);
       std::vector<char> temp;
       temp.reserve(50);
+      _currentSize=0;
       while ( !_fileStream->IsAtEnd() && _currentSize < _chunkSize)
       {
         const char c = _fileStream->Get();
