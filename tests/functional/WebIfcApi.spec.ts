70333af4f15f308bf92a40815cdaf38f8fb8319c
Tom Beach <beachth@cf.ac.uk>
2023-10-10 10:57:10 +0100

    Update WebIfcApi.spec.ts

--- tests/functional/WebIfcApi.spec.ts
+++ tests/functional/WebIfcApi.spec.ts
@@ -3,50 +3,6 @@ import * as path from 'path';
 import * as WebIFC from '../../dist/web-ifc-api-node.js';
 import {IFC2X3} from '../../dist/web-ifc-api-node.js';
 import {LoaderSettings,IfcLineObject} from '../../dist/web-ifc-api-node.js';
-/**
- * Should be somewhere else.
- */
-function Utf8ArrayToStr(array: Uint8Array) {
-    var out, i, len, c;
-    var char2, char3;
-
-
-    out = "";
-    len = array.length;
-    i = 0;
-    while (i < len) {
-        c = array[i++];
-        switch (c >> 4) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-                // 0xxxxxxx
-                out += String.fromCharCode(c);
-                break;
-            case 12:
-            case 13:
-                // 110x xxxx   10xx xxxx
-                char2 = array[i++];
-                out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-                break;
-            case 14:
-                // 1110 xxxx  10xx xxxx  10xx xxxx
-                char2 = array[i++];
-                char3 = array[i++];
-                out += String.fromCharCode(((c & 0x0F) << 12) |
-                    ((char2 & 0x3F) << 6) |
-                    ((char3 & 0x3F) << 0));
-                break;
-        }
-    }
-
-    return out;
-}
 
 import type {
     Vector,
