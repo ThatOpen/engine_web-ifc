cfdf54d107e0d59bcb4b3767210753304bdf2884
Tom Beach <beachth@cf.ac.uk>
2024-07-01 16:10:23 +0100

    Closes #884

--- src/schema-generator/schema_aliases.ts
+++ src/schema-generator/schema_aliases.ts
@@ -13,7 +13,8 @@ const schemaAliases : Array<SchemaAlias>  = [
 { schemaName : "IFC4X3_RC4", alias :"IFC4X3"},
 { schemaName : "IFC4X3_RC1", alias :"IFC4X3"},
 { schemaName : "IFC4X3_RC2", alias :"IFC4X3"},
-{ schemaName : "IFC4X3_ADD2", alias :"IFC4X3"}
+{ schemaName : "IFC4X3_ADD2", alias :"IFC4X3"},
+{ schemaName : "IFC4X3_ADD1", alias :"IFC4X3"}
 
 ];
 
