6224d5389e512dfc9f01f99ddc5671e7eb86774c
liradb2000 <liradb2000@live.co.kr>
2025-02-18 10:59:43 +0900

    refactor: remove mainScriptUrlOrBlob parameter from wasmModule initialization

--- src/ts/web-ifc-api.ts
+++ src/ts/web-ifc-api.ts
@@ -223,7 +223,7 @@ export class IfcAPI {
             }
 
             //@ts-ignore
-            this.wasmModule = await WebIFCWasm({ noInitialRun: true, locateFile: customLocateFileHandler || locateFileHandler,mainScriptUrlOrBlob });
+            this.wasmModule = await WebIFCWasm({ noInitialRun: true, locateFile: customLocateFileHandler || locateFileHandler });
             this.SetLogLevel(LogLevel.LOG_LEVEL_ERROR);
         }
         else {
